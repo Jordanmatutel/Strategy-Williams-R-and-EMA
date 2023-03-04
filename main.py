4fbf59bd8f6c8fb573e87da92594814b32fc696e
Jordanmatutel <donjordanje@gmail.com>
2023-03-04 12:09:13 -0600

    Improved, bugs fixed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import ccxt
+import numpy as np
 import pandas as pd
 
 
@@ -17,11 +18,14 @@ def williamsCalculator(highest, lowest, closing_prices):
     return williams_list
 
 def trendDetector(closing_prices):
-    if closing_prices[0] > closing_prices[1]:
-        trend = True
-    else:
-        trend = False
-    return trend
+    nparray = np.array(closing_prices)
+    sma10 = np.mean(nparray[-10])
+    sma20 = np.mean(nparray[-20])
+    if sma10 > sma20:
+        f = True
+    elif sma10 < sma20:
+        f = False
+    return f
 
 def Signal(williams, EMA, trend):
     signal = williams[0] - EMA[0]
@@ -43,31 +47,34 @@ while True:
     exchange = ccxt.binance({"Public Key": "XXXXX", "Secret Key": "XXXXX"})
     prices = exchange.fetch_ohlcv(symbol=market, timeframe="1m", limit=williams_lenght)
 
-    closing_prices = [candle[4] for candle in prices]
-    closing = pd.DataFrame(closing_prices)
-
     highest_prices = [candle[2] for candle in prices]
     highest = pd.DataFrame(highest_prices)
 
     lowest_prices = [candle[3] for candle in prices]
     lowest = pd.DataFrame(lowest_prices)
 
+    closing_prices = [candle[4] for candle in prices]
+    closing = pd.DataFrame(closing_prices)
+
     #Now we calculate the EMA of the Williams %R and we add it to our list.
     williams_list = williamsCalculator(highest, lowest, closing)
     ema = williams_list[0].ewm(alpha=1, adjust=False).mean()
     trend = trendDetector(closing_prices)
     signal = Signal(williams_list, ema, trend)
+    williams = williams_list[0]
+    williams = williams[0]     # Take the last Williams %R value
 
     position = False
-    entryCall = 0
-    closePrice = 0
-    profitSum = (entryCall - closePrice) * leverage
+    entry = 0
+    close = 0
+    profitSum = (entry - close) * leverage
 
-    if Signal and position:
-        lastCall = closing_prices[0]
+    if Signal and position == False:
+        entry = closing_prices[0]
+        position = True
 
     if position and not Signal:
-        closePrice = closing_prices[0]
+        close = closing_prices[0]
         position = False
 
     print("The total of profit:", profitSum)
