7c0c8a7165f63dedf8fe2bcb1fa2b74b51d6984c
Jordanmatutel <donjordanje@gmail.com>
2023-03-05 01:53:30 -0600

    Details added

---
+++ calculator.py
@@ -0,0 +1,36 @@
+import numpy as np
+
+
+def williamsCalculator(highest, lowest, closing_prices):
+    williams_list = [0] * len(highest)
+    highest_high = highest.max()
+    highest_high = float(highest_high)
+    lowest_low = lowest.min()
+    lowest_low = float(lowest_low)
+    last_close = closing_prices[0]
+    # We calculate the Williams %R and we save it in one dataframe.
+    williams = ((last_close - highest_high) / (highest_high - lowest_low)) * 100
+    williams_list[1:] = williams_list[:-1]
+    williams_list[0] = williams
+    return williams_list
+
+
+def trendDetector(closing_prices):
+    nparray = np.array(closing_prices)
+    sma10 = np.mean(nparray[-10])
+    sma20 = np.mean(nparray[-20])
+    if sma10 > sma20:
+        f = True
+    elif sma10 < sma20:
+        f = False
+    return f
+
+
+def Signal(williams, EMA, trend):
+    signal = williams[0] - EMA[0]
+    if signal.between(-0.10, 0.10).all():
+        if trend:
+            buy = True
+    else:
+        buy = False
+    return buy
--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 import time
 import ccxt
-import numpy as np
 import pandas as pd
-
-
-def williamsCalculator(highest, lowest, closing_prices):
-    williams_list = [0] * len(highest)
-    highest_high = highest.max()
-    highest_high = float(highest_high)
-    lowest_low = lowest.min()
-    lowest_low = float(lowest_low)
-    last_close = closing_prices[0]
-    #We calculate the Williams %R and we save it in one dataframe.
-    williams = ((last_close - highest_high) / (highest_high - lowest_low)) * 100
-    williams_list[1:] = williams_list[:-1]
-    williams_list[0] = williams
-    return williams_list
-
-def trendDetector(closing_prices):
-    nparray = np.array(closing_prices)
-    sma10 = np.mean(nparray[-10])
-    sma20 = np.mean(nparray[-20])
-    if sma10 > sma20:
-        f = True
-    elif sma10 < sma20:
-        f = False
-    return f
-
-def Signal(williams, EMA, trend):
-    signal = williams[0] - EMA[0]
-    if signal.between(-0.10, 0.10).all():
-        if trend:
-            buy = True
-    else:
-        buy = False
-    return buy
+import calculator as clc
 
 leverage = 3
 market = "BTC/USDT"
@@ -56,24 +23,24 @@ while True:
     closing_prices = [candle[4] for candle in prices]
     closing = pd.DataFrame(closing_prices)
 
-    #Now we calculate the EMA of the Williams %R and we add it to our list.
-    williams_list = williamsCalculator(highest, lowest, closing)
+    # Now we calculate the EMA of the Williams %R and we add it to our list.
+    williams_list = clc.williamsCalculator(highest, lowest, closing)
     ema = williams_list[0].ewm(alpha=1, adjust=False).mean()
-    trend = trendDetector(closing_prices) #Use the SMA to verify the trend
-    signal = Signal(williams_list, ema, trend)
+    trend = clc.trendDetector(closing_prices)  # Use the SMA to verify the trend
     williams = williams_list[0]
     williams = williams[0]     # Take the last Williams %R value
+    signal = clc.Signal(williams, ema, trend)
 
     position = False
     entry = 0
     close = 0
     profitSum = (entry - close) * leverage
 
-    if Signal and position == False:
+    if signal and position == False:
         entry = closing_prices[0]
         position = True
 
-    if position and not Signal:
+    if position and not signal:
         close = closing_prices[0]
         position = False
 
